7c06a4e18b6bc9c0149f9efb71b34ad49c6cc12a
Peter Maag <maag.peter@gmail.com>
2013-04-20 20:46:52 -0600

    initial commit

---
+++ workers/iron_texter.rb
@@ -0,0 +1,18 @@
+require 'twilio-ruby'
+require 'json'
+
+# Parse webhook
+@payload = JSON.parse(payload)
+
+# Load configuration
+config = YAML.load_file("settings.yml")
+@client = Twilio::Rest::Client.new config['twilio']['account_sid'] config['twilio']['auth_token']
+
+# Send SMS
+@client.account.sms.messages.create(
+	:from => config['twilio']['phone_number'],
+	:to => @payload["destination"],
+	:body => "BTC traded at #{@payload["price"]} which is \
+		#{@payload["alert_when"].downcase} your threshold \
+		of #{@payload["threshold"]}."
+)
